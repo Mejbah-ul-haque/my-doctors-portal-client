50d0dcb433b96c92ee91af3293f73b24547c3456
Mejbah <mejba.s@gmail.com>
2022-05-10 05:33:18 +0600

    home and about component included

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import Navbar from "./Pages/Shared/Navbar";
 import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
 
 function App() {
   return (
