1698df76ec90463190eabf343183c27f07950352
Mejbah <mejba.s@gmail.com>
2022-05-10 04:58:27 +0600

    daisyui theme customization added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
   daisyui: {
     themes: [
       {
-        doctortheme: {
-          primary: "#a991f7",
-          secondary: "#f6d860",
-          accent: "#37cdbe",
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
           neutral: "#3d4451",
           "base-100": "#ffffff",
         },
       },
-      "dark",
-      "cupcake",
     ],
   },
+  plugins: [require("daisyui")],
 }
