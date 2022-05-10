4be66a010317600efd41e7f6add1e4138106b796
Mejbah <mejba.s@gmail.com>
2022-05-11 04:22:59 +0600

    Date picker added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import chair from "../../assets/images/chair.png";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { format } from "date-fns";
 
 const AppointmentBanner = () => {
+  const [date, setDate] = useState(new Date());
   return (
-    <div>
-      <h2></h2>
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={chair}
+          className="max-w-sm rounded-lg shadow-2xl"
+          alt="Dentist Chair"
+        />
+        <div>
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
+          <p>You have selected: {format(date, "PP")}</p>
+        </div>
+      </div>
     </div>
   );
 };
