c766247ec86cfffc83fda298c41be56a8dcba4b4
Mejbah <mejba.s@gmail.com>
2022-05-11 06:49:06 +0600

    dynamic appointment cards added

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,22 +4,22 @@ const Service = ({ service }) => {
   const { name, slots } = service;
   return (
     <div className="card lg:max-w-lg bg-base-100 shadow-xl">
-      <div className="card-body ">
-        <h2 className="card-title">{name}</h2>
+      <div className="card-body text-center">
+        <h2 className="card-title text-secondary">{name}</h2>
         <p>
           {slots.length > 0 ? (
             <span>{slots[0]}</span>
           ) : (
-            <span className="text-red-500">No Slot Available</span>
+            <span className="text-red-500">Try Another Day!</span>
           )}
         </p>
         <p>
           {slots.length} {slots.length > 1 ? "spaces" : "space"} available
         </p>
-        <div className="card-actions justify-end">
+        <div className="card-actions justify-center">
           <button
             disabled={slots.length === 0}
-            className="btn btn-primary uppercase"
+            className="btn btn-secondary text-white uppercase"
           >
             Book Appointment
           </button>
