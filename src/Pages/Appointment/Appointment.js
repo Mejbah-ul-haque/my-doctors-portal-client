1f32769eadbb0afaef7c882a5be1cbbaa52c3ab6
Mejbah <mejba.s@gmail.com>
2022-05-11 07:23:02 +0600

    Modal implementation

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import Footer from "../Shared/Footer";
 import AppointmentBanner from "./AppointmentBanner";
-import AvailableAppoinments from "./AvailableAppoinments";
+import AvailableAppointments from "./AvailableAppointments";
 
 const Appointment = () => {
   const [date, setDate] = useState(new Date());
   return (
     <div>
       <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
-      <AvailableAppoinments
+      <AvailableAppointments
         date={date}
         setDate={setDate}
-      ></AvailableAppoinments>
+      ></AvailableAppointments>
       <Footer></Footer>
     </div>
   );
